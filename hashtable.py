5f022907f44b1f49c06451d39f846ac822c3397e
Matthew <matthewna25@gmail.com>
2025-05-20 10:45:21 +0800

    add hashtable implementation

--- hashtable.py
+++ hashtable.py
@@ -1,6 +1,7 @@
 class HashTable:
     def __init__(self, size):
-        pass
+        self.size = size
+        self.values = size*[None]
 
     def __repr__(self):
         """returns a formatted string containing the values in the hash table"""
@@ -20,19 +21,68 @@ class HashTable:
         - key: str
           The key to be hashed
         """
-        pass
+        p = 31
+        m = 1000000009
+        
+        hashValue = 0
+        pPow = 1
+        
+        for i in range(len(key)):
+          charValue = ord(key[i]) - ord('a') + 1
+          hashValue = (hashValue + charValue * pPow) % m
+          pPow = (pPow * p) % m
+        
+        return hashValue % self.size
 
     def setitem(self, key: str, value: dict) -> None:
         """
+        Adds / Updates the key in the hash table with the value.
+        
+        Parameters
+        -----------
+        - key: str
+          The key to be used in the hash table.
+        - value: dict
+          The record to be inserted.
         """
-        pass
+        index = self._hash(key)
+        if self.values[index] is None:
+          print('Data successfully added!')
+        else:
+          print('Data successfully updated!')
+          
+        self.values[index] = value
 
     def getitem(self, key: str) -> 'dict | None':
         """
+        Returns the value associated with the key in the hash table.
+        
+        Parameters
+        -----------
+        - key: str
+          The key to be used in the hash table.
         """
-        pass
+        index = self._hash(key)
+        if self.values[index] is None:
+          print('Destination is empty!')
+          return None
+        
+        return self.values[index]
         
     def delitem(self, key: str) -> None:
         """
+        Deletes the key-pair value in the hash table, if it exists.
+        
+        Parameters
+        -----------
+        - key: str
+          The key to be used in the hash table.
         """
-        pass
+        index = self._hash(key)
+        if self.values[index] is None:
+          print('Unable to remove. Destination is empty!')
+          return
+        
+        self.values[index] = None
+        print('Data successfully removed')
+      
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from hashtable import HashTable
+import csv
 
 if __name__ == "__main__":
     ht = HashTable(30)
@@ -11,8 +12,23 @@ if __name__ == "__main__":
     2. You can use the id as the hash table key for 
     each of the above records.
     """
+    with open('student_data.csv', 'r') as f:
+      reader = csv.DictReader(f)
+      for row in reader:
+        key = row['id']
+        ht.setitem(key, row)
+      
+      # f.close() runs automatically
+    
     
     # Test your hashtable using appropriate methods
     # from your implementation
+    
+    print(ht.getitem('s0002b'))
+    print(ht.getitem('s0021e'))
+    
+    print(ht.getitem('s0011a'))
+    ht.delitem('s0011a')
+    print(ht.getitem('s0011a'))
     
     
